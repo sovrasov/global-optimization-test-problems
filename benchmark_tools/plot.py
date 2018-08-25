b13bbec78e016fdf3e7f0f27b6780b828f67c0d7
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2018-08-25 13:41:51 +0300

    Add contour lines plotting

--- benchmark_tools/plot.py
+++ benchmark_tools/plot.py
@@ -41,3 +41,60 @@ def plot_cmcs(curves, captures=None, show=True, filename=None):
 
     if show:
         plt.show()
+
+def plot_countours(functions, bounds, filename, show=False, points=None):
+    lb, ub = bounds
+    assert len(lb) == len(ub) == 2
+    assert len(functions) > 0
+
+    n_cells = 300
+    x = np.linspace(lb[0], ub[0], n_cells, endpoint=True)
+    y = np.linspace(lb[1], ub[1], n_cells, endpoint=True)
+    X, Y = np.meshgrid(x, y, indexing='ij')
+
+    Z_obj = np.zeros((n_cells, n_cells), dtype=np.float)
+    for i in range(len(X)):
+        for j in range(len(Y)):
+            Z_obj[i, j] = functions[-1]([X[i, j], Y[i, j]])
+
+    plt.figure()
+    if points:
+        points = np.array(points)
+        plt.scatter(points[:,0], points[:,1], c='r')
+    matplotlib.rcParams['contour.negative_linestyle'] = 'solid'
+    CS = plt.contour(X, Y, Z_obj, colors='k', extent=[lb[0],ub[0],lb[1],ub[1]])
+
+    n_constraints = len(functions) - 1
+    if n_constraints > 0:
+        Z_c = np.zeros((n_cells, n_cells), dtype=np.float)
+        for i in range(len(X)):
+            for j in range(len(Y)):
+                val = -np.inf
+                for f in functions[:-1]:
+                    val = f([X[i, j], Y[i, j]])
+                    if val > 0:
+                        break
+                Z_c[i, j] = val
+        CS_c = plt.contourf(X, Y, Z_c, [-np.inf, 0], colors=['green'], \
+                            alpha=.4, extent=[lb[0],ub[0],lb[1],ub[1]])
+
+        for f in functions[:-1]:
+            for i in range(len(X)):
+                for j in range(len(Y)):
+                    Z_c[i, j] = f([X[i, j], Y[i, j]])
+            plt.contour(X, Y, Z_c, [0], colors='k', extent=[lb[0],ub[0],lb[1],ub[1]])
+
+    plt.clabel(CS, inline=1, fontsize=10)
+    plt.xlabel(r'$x$', fontsize=15)
+    plt.ylabel(r'$y$', fontsize=15)
+    plt.xticks(fontsize=13)
+    plt.yticks(fontsize=13)
+    plt.xlim([lb[0], ub[0]])
+    plt.ylim([lb[1], ub[1]])
+    plt.grid()
+
+    if len(filename) > 0:
+        plt.savefig(filename, dpi=300)
+
+    if show:
+        plt.show()
