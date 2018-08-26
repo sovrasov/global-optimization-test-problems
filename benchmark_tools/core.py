16539712392eeb5f7f2ffb4b7f4d2ddb5662b4ae
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2018-08-26 12:50:58 +0300

    Fix solution check in solve_set

--- benchmark_tools/core.py
+++ benchmark_tools/core.py
@@ -7,22 +7,36 @@ class Solver:
     def Solve(self, problem):
         return None
 
-def solve_class(problems_set, solver, verbose=False, eps_check=0.01):
+def solve_class(problems_set, solver, verbose=False, eps_check=0.01, relative_eps=True, external_solution_check=True):
+    assert eps_check > 0
     all_calcs = []
     solved_satus = []
     for i, problem in enumerate(problems_set.get_problems()):
-        opt_point, opt_val, calcs = solver.Solve(problem)
-        ref_point = np.array(problem.GetOptimumPoint())
-        diff = np.linalg.norm(opt_point - ref_point, np.inf)
-        if diff < eps_check:
-            solved_satus.append(True)
+        if relative_eps:
+            bounds = problem.GetBounds()
+            eps = np.linalg.norm(bounds[0] - np.array(bounds[1]), np.inf)*eps_check
         else:
-            solved_satus.append(False)
+            eps = eps_check
+        if external_solution_check:
+            problem.SetEps(eps)
+
+        opt_point, opt_val, calcs = solver.Solve(problem)
+
+        if external_solution_check:
+            solved_satus.append(problem.IsStatsUpdateStopped())
+        else:
+            ref_point = np.array(problem.GetOptimumPoint())
+            diff = np.linalg.norm(opt_point - ref_point, np.inf)
+            if diff < eps:
+                solved_satus.append(True)
+            else:
+                solved_satus.append(False)
+
         all_calcs.append(calcs)
 
         if verbose:
             print('Problem # {}: '.format(i + 1) + ('solved' if solved_satus[-1] else 'not solved') +
-                    ' after {} iterations'.format(calcs[0]))
+                    ' after {} trials'.format(calcs[0]))
 
     return all_calcs, solved_satus
 
